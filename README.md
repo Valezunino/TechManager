f1e11efc630eec88b74725e25a8aecf755291de9
Mateo Preciado <mateo.preciado9@gmail.com>
2026-09-01 18:35:11 -0300

    Limpieza completa de la rama

--- README.md
+++
@@ -1,3 +0,0 @@
-# TechManager
-
-Repositorio principal del proyecto TechManager. El trabajo de cada integrante se organiza en ramas individuales.
--- src/main/java/com/techmanager/techmanager/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techmanager.techmanager.repository;
-
-import com.techmanager.techmanager.entity.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-}
